f106084f9020826b0940b65e862eb977dd26d37e
Raz Ashkenazi <razash10@gmail.com>
2025-02-01 16:00:40 +0200

    Code refactoring

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 const container = document.createElement("div");
 container.className = "flex-container";
+
 for (let i = 0; i < 16; i++) {
     const cell = document.createElement("div");
     container.appendChild(cell);
 }
-const body = document.querySelector("body");
-body.appendChild(container);
+
+document.body.appendChild(container);
