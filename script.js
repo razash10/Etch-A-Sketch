e7b48dbbe56b6adb068e6d600c65f6b1f3d893ee
Raz Ashkenazi <razash10@gmail.com>
2025-02-01 17:35:25 +0200

    Added hover effect on the grid

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+function onHover(event) {
+    event.target.style.backgroundColor = "black";
+    event.target.style.opacity = parseFloat(event.target.style.opacity || 0.05) + 0.05;
+}
 
 const container = document.createElement("div");
 container.className = "flex-container";
 
 for (let i = 0; i < 16; i++) {
     const cell = document.createElement("div");
+    cell.addEventListener("mouseover", onHover)
     container.appendChild(cell);
 }
 
